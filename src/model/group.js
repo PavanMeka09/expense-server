35a6057a04e411aa3f13eb98b1ed9977cff0214e
PavanMeka09 <Mekapavan99@gmail.com>
2026-02-11 13:38:32 +0530

    fix group date bug

--- src/model/group.js
+++ src/model/group.js
@@ -4,7 +4,7 @@ const groupSchema = new mongoose.Schema({
     name: { type: String, required: true},
     description: { type: String, required: false },
     adminEmail: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now() },
+    createdAt: { type: Date, default: Date.now },
     membersEmail: [String],
     thumbnail: { type: String, required: false },
     paymentStatus: {
